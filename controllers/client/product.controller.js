e3f3ecd30b5d98fca3b99bc991c6bef8bd2dcc08
leminhtan <tinh7demtb@gmail.com>
2026-01-17 21:16:54 +0700

    Tính năng xem thông tin sp bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -4,8 +4,8 @@ const Product = require('../../models/product.model')
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         'deleted':false, 
-        'status':'active' 
-    });
+        'status':'active',
+    }).sort({ position: "desc" });
 
     const newProduct = products.map(item => {
         item.newPrice = (item.price*(100-item.discountPercentage)/100).toFixed(1);
@@ -15,5 +15,19 @@ module.exports.index = async (req, res) => {
     res.render('client/pages/products/index.pug', {
         pageTitle: "Trang sản phẩm",
         products: newProduct,
+    })
+}
+
+//[GET] /products/detail/slug
+module.exports.detail = async (req,res) => {
+    const find = {
+        deleted: false,
+        slug: req.params.slug,
+        status: "active",
+    }
+    const product = await Product.findOne(find)
+    res.render("client/pages/products/detail",{
+        pageTitle: product.title,
+        product: product,
     })
 }
--- routes/client/index.products.js
+++ routes/client/index.products.js
@@ -5,4 +5,6 @@ const productController = require('../../controllers/client/product.controller')
 
 router.get('/', productController.index)
 
+router.get('/detail/:slug', productController.detail)
+
 module.exports = router;
