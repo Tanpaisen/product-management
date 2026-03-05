5a076d6d490e556b0fb24b51b92de512bd3e03ca
leminhtan <tinh7demtb@gmail.com>
2026-03-05 11:01:37 +0700

    Tạo bảng thuộc tính accout

---
+++ models/account.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const generate = require('../helper/generateRandomString');
+const accountShema = new mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    token: {
+      type: String,
+      default: generate.generateRandomString(20),
+    },
+    phone: String,
+    avatar: String,
+    role_id: String,
+    status: String,
+    deleted: {
+        type: Boolean,
+        default: false,
+    },
+    deleteAt: Date,
+}, {
+    timestamps: true
+});
+const Account = mongoose.model('Account', accoutSchema, account)
+
+module.exports = Account;
