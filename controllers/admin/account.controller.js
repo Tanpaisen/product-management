cc16733dddd6fe679b4d8f95a6a1a2f9ae6e1c7c
leminhtan <tinh7demtb@gmail.com>
2026-03-06 13:56:55 +0700

    update chức năng hiển thị danh sách tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -10,6 +10,13 @@ const systemConfig = require('../../config/system')
 module.exports.index = async (req, res) => {
     const records = await Account.find();
     
+    for(const record of records){
+        const role = await Role.findOne({
+            _id: record.role_id,
+            deleted: false,
+        });
+        record.role = role;
+    }
     res.render('admin/pages/accounts/index', {
         pageTitle: "Trang quản trị tài khoản",
         records: records,
