a27d5b3529b97d5a5aad88359270e847f059b601
Tanpaisen <tinh7demtb@gmail.com>
2026-03-24 20:45:04 +0700

    Đổ dữ liệu ra trang danh sách

--- controllers/admin/blog.controller.js
+++ controllers/admin/blog.controller.js
@@ -3,8 +3,9 @@ const Blog = require('../../models/blog.model')
 //[GET] /
 module.exports.index = async (req, res) => {
     const blogs = await Blog.find({deleted: false})
-    console.log()
+    
     res.render('admin/pages/blogs/index.pug', {
-        pageTitle: 'Trang quản lý bài viết'
+        pageTitle: 'Trang quản lý bài viết',
+        blogs: blogs,
     })
 }
