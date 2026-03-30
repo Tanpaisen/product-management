9c591403d77210007c7c650bdbd7e973d27cc4be
Tanpaisen <tinh7demtb@gmail.com>
2026-03-30 17:37:35 +0700

    danh sách sản phẩm mới nhất bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -5,10 +5,20 @@ const priceProductHelper = require('../../helper/price')
 //[GET] /
 module.exports.index = async (req, res) => {
 
-    const productsFeatured = await Product.find({deleted: false, featured: "1", status: "active"}).sort({ position: "desc" });
+    const productsFeatured = await Product.find({ deleted: false, featured: "1", status: "active" }).sort({ position: "desc" });
     const newProductsFeatured = priceProductHelper.newPriceProducts(productsFeatured)
+
+    const productsNew = await Product
+        .find({
+            deleted: false, 
+            status: "active"
+        })
+        .sort({ position: "desc" })
+        .limit(6);
+    const newProductsNew = priceProductHelper.newPriceProducts(productsNew)
     res.render('client/pages/home/index.pug', {
         pageTitle: "Trang chủ",
-        productsFeatured: newProductsFeatured
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     })
 }
