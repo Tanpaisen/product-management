9f65034c55e4d6d924bbf004e6dc439a9909585a
Tanpaisen <tinh7demtb@gmail.com>
2026-04-05 21:27:23 +0700

    Thêm minicart bên client

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -7,5 +7,14 @@ module.exports.cart = async (req, res, next) => {
         const time = 365 * 24 * 60 * 60 * 1000;
         res.cookie('cartId',cart.id, { expires: new Date(Date.now() + time), httpOnly: true })
     }
+    else{
+        const carts = await Cart.find()
+        
+        for(cart of carts){
+            cart.totalQuantity = cart.products.reduce((sum,item) => sum+item.quantity,0)
+        }
+        res.locals.cart = cart
+        
+    }
     next();
 }
